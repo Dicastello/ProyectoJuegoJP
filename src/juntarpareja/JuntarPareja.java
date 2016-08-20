6e6b09163307f12c6634f6a841bbe03e2ff0a1b7
MeraliCastillo <MeraliCastillo@Merali>
2016-08-19 23:09:44 -0600

    Prueba juntarpareja.java

--- src/juntarpareja/JuntarPareja.java
+++ src/juntarpareja/JuntarPareja.java
@@ -16,6 +16,7 @@ public class JuntarPareja {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola");
         Principal p = new Principal();
         p.setVisible(true);
     }
