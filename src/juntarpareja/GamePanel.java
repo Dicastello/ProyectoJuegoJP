c84ed3dfc9efefaec59adcb167f24b9affe202f2
MeraliCastillo <MeraliCastillo@Merali>
2016-09-02 22:26:37 -0600

    Actualizacion beta 0.8

--- src/juntarpareja/GamePanel.java
+++ src/juntarpareja/GamePanel.java
@@ -23,9 +23,10 @@ import javax.swing.JPanel;
  */
 public class GamePanel extends JPanel implements MouseListener{
      private JPanel p2;
-    private JLabel l[]= new JLabel[16];
-    private int ArreNumerico[]=new int[16];
-    private int controlRepeticiones[]=new int[8];
+    private JLabel l[]= new JLabel[8];
+    private JLabel Reiniciar[]= new JLabel[8];
+    private int ArreNumerico[]=new int[8];
+    private int controlRepeticiones[]=new int[4];
     private JButton Rei;
 
     public GamePanel() {
@@ -38,37 +39,47 @@ public class GamePanel extends JPanel implements MouseListener{
             add(Rei,BorderLayout.EAST);
             //Panel del tablero
             p2= new JPanel();
-            p2.setLayout(new GridLayout(4,4,4,4));            
+            p2.setLayout(new GridLayout(2,2));            
             add(p2,BorderLayout.CENTER);
-        InicializarComponentes();
+             LlenadoLabel();
+             InicializarComponentes();
     }
-
+        
     private void InicializarComponentes() {
          //Panel de las estadisticas
-            
-            LlenadoLabel();
+        
+            ArregloImagenes();
             RandonNumeros();
             InicializarArreglo(ArreNumerico);
             InicializarArreglo(controlRepeticiones);
            
             
     }
-    //Panel del tablero
-    private void LlenadoLabel(){
-     
+    //Metodo para armar el Panel del tablero 
+    private void LlenadoLabel(){          
         for (int i = 0; i < l.length; i++) {
-            l[i]=new JLabel(); 
-            l[i].setIcon(new ImageIcon(getClass().getResource("/Imagen/Buscar.jpg"))); 
-            l[i].addMouseListener(this);
+            l[i]=new JLabel();
+            l[i].setIcon(new ImageIcon(getClass().getResource("/Imagen/Buscar.jpg")));
+            
             p2.add(l[i]);
-        }
+        }       
     }
+    // Metodo pare reiniciar las imagines del juego
+     private void ArregloImagenes(){
+        for (int i = 0; i <Reiniciar.length; i++) {
+            Reiniciar[i]=l[i];
+        }
+        for (int i = 0; i < Reiniciar.length; i++) {
+            Reiniciar[i].setIcon(new ImageIcon(getClass().getResource("/Imagen/Buscar.jpg")));             
+            Reiniciar[i].addMouseListener(this);
+        }
+   }
     //Metodo para generar los numeros aleatorios para cargar las imagenes en los JLeabel
     private void RandonNumeros(){
         int PosicionImagen;
         Random r= new Random();
         for (int i = 0; i < ArreNumerico.length; i++) {
-            PosicionImagen=r.nextInt(8);
+            PosicionImagen=r.nextInt(4);
             if (controlRepeticiones[PosicionImagen]<2) {
                 ArreNumerico[i]=PosicionImagen+1;   
                 controlRepeticiones[PosicionImagen]++;
@@ -82,16 +93,17 @@ public class GamePanel extends JPanel implements MouseListener{
             System.out.print(controlRepeticiones[i]+"");
         }
     }
-
+    
     @Override
-    public void mouseClicked(MouseEvent e) throws UnsupportedOperationException{
+    public void mouseClicked(MouseEvent e){
         if (e.getSource()==Rei) {  
-           
+                
               InicializarComponentes();
             }
-        for (int i = 0; i < l.length; i++) {
-            if (e.getSource()==l[i]) {  
-                l[i].setIcon(this.AjustarImagen("/Imagen/ok.jpg")); 
+        for (int i = 0; i < Reiniciar.length; i++) {
+            if (e.getSource()==Reiniciar[i]) {  
+                l[i].setIcon(this.AjustarImagen("/Imagen/"+Reiniciar[i]+".jpg")); 
+                
             }
         }
     }
@@ -121,5 +133,7 @@ public class GamePanel extends JPanel implements MouseListener{
              this.repaint();
            return icono; 
    }
-    
+   
+   
 }
+ 
