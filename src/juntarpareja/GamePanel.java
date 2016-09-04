73e375d1f38be620db1b27c8008c4c6f0ea4f92f
MeraliCastillo <MeraliCastillo@Merali>
2016-09-03 18:36:03 -0600

    Actualizacion beta 1.0

--- src/juntarpareja/GamePanel.java
+++ src/juntarpareja/GamePanel.java
@@ -11,6 +11,7 @@ import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Random;
+import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -22,46 +23,44 @@ import javax.swing.JPanel;
  * @author MeraliCastillo
  */
 public class GamePanel extends JPanel implements MouseListener{
-     private JPanel p2;
+     private JPanel Tablero,HistorialJ,JugaadorTop;
     private JLabel l[]= new JLabel[8];
     private JLabel Reiniciar[]= new JLabel[8];
     private int ArreNumerico[]=new int[8];
     private int controlRepeticiones[]=new int[4];
     private JButton Rei;
-
+    private int Click=0;
+    private int Vista1=0;
+    private int vista2=0;
+    
+    
     public GamePanel() {
-        setLayout(new BorderLayout(100,1));
-            JLabel Esta= new JLabel("Prueba");
-            add(Esta,BorderLayout.EAST);
-            Rei=new JButton();
-            Rei.setText("ONAN");
-            Rei.addMouseListener(this);
-            add(Rei,BorderLayout.EAST);
-            //Panel del tablero
-            p2= new JPanel();
-            p2.setLayout(new GridLayout(2,2));            
-            add(p2,BorderLayout.CENTER);
+                     
+          //  Panel del tablero
+            Tablero= new JPanel();
+            Tablero.setLayout(new GridLayout(2,2));            
+            add(Tablero,BorderLayout.CENTER);
+            HistorialDeJugdor();
+            TopJugdores();
              LlenadoLabel();
              InicializarComponentes();
     }
         
     private void InicializarComponentes() {
-         //Panel de las estadisticas
-        
+         //Panel de las estadisticas        
             ArregloImagenes();
-            RandonNumeros();
             InicializarArreglo(ArreNumerico);
             InicializarArreglo(controlRepeticiones);
-           
+            RandonNumeros();
+     
             
     }
     //Metodo para armar el Panel del tablero 
     private void LlenadoLabel(){          
         for (int i = 0; i < l.length; i++) {
             l[i]=new JLabel();
-            l[i].setIcon(new ImageIcon(getClass().getResource("/Imagen/Buscar.jpg")));
-            
-            p2.add(l[i]);
+            l[i].setIcon(new ImageIcon(getClass().getResource("/Imagen/Buscar.jpg")));          
+            Tablero.add(l[i]);
         }       
     }
     // Metodo pare reiniciar las imagines del juego
@@ -101,8 +100,21 @@ public class GamePanel extends JPanel implements MouseListener{
               InicializarComponentes();
             }
         for (int i = 0; i < Reiniciar.length; i++) {
-            if (e.getSource()==Reiniciar[i]) {  
-                l[i].setIcon(this.AjustarImagen("/Imagen/"+Reiniciar[i]+".jpg")); 
+            if (e.getSource()==Reiniciar[i]) { 
+                Click++;
+                System.out.println(""+Click);
+                Reiniciar[i].setIcon(AjustarImagen("/Imagen/"+ArreNumerico[i]+".jpg")); 
+                if(Click==1){
+                    Vista1=i;   
+                           System.out.println("Vista 1"+Vista1);
+                           System.out.println("posicion"+i);
+                }else{
+                    vista2=i;
+                           System.out.println("Vista 1"+vista2);
+                           System.out.println("posicion"+i);
+                }
+         
+                System.out.println(""+i);
                 
             }
         }
@@ -116,9 +128,18 @@ public class GamePanel extends JPanel implements MouseListener{
 
     @Override
     public void mouseEntered(MouseEvent e) throws UnsupportedOperationException{}
-
+    //metodo para validar en cotro las parejas
     @Override
-    public void mouseExited(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {
+        for (int i = 0; i < Reiniciar.length; i++) {
+            if (e.getSource()==Reiniciar[i]) {
+                if (Click==2) {
+                    ValidarPareja(Vista1,vista2);
+                    Click=0;
+                }
+            }
+        }
+    }
     //Se Nicializan en O
     private void InicializarArreglo(int[] Arreglo) {
         for (int i = 0; i < Arreglo.length; i++) {
@@ -130,9 +151,35 @@ public class GamePanel extends JPanel implements MouseListener{
            ImageIcon fot = new ImageIcon(getClass().getResource(ruta));
             Icon icono = new ImageIcon(fot.getImage().getScaledInstance(l[0].getWidth(),l[0].getHeight(), Image.SCALE_DEFAULT));
             //jLabel1.setIcon(icono);
-             this.repaint();
+            // this.repaint();
            return icono; 
    }
+
+    private void TopJugdores() {
+      JLabel prueba= new JLabel("Prueba");   
+    }
+    //Llenado el historial del jugador
+    private void HistorialDeJugdor() {
+            JLabel Esta= new JLabel("Prueba");   
+            HistorialJ= new JPanel();
+            HistorialJ.add(Esta);
+            Rei=new JButton();
+            Rei.setText("ONAN");
+            Rei.addMouseListener(this);
+            HistorialJ.add(Rei);
+            HistorialJ.setLayout(new BoxLayout(HistorialJ,BoxLayout.Y_AXIS));            
+            add(HistorialJ,BorderLayout.EAST);
+    }
+
+    private void ValidarPareja(int Vist, int vist2) {
+        if (Reiniciar[Vist]==Reiniciar[vist2]) {
+            
+        }else{
+            Reiniciar[Vist].setIcon(new ImageIcon(getClass().getResource("/Imagen/Buscar.jpg")));
+            Reiniciar[vist2].setIcon(new ImageIcon(getClass().getResource("/Imagen/Buscar.jpg")));
+            
+        }
+    }
    
    
 }
